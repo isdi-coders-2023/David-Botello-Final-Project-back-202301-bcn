f8d42904b9caa6f9bcddde291b6391aa104b94c0
David Botello <dabocher@gmail.com>
2023-03-07 14:52:11 +0100

    Add cors origins

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,21 @@
+import "./loadEnviroment.js";
+
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
+const allowedOrigins = [
+  process.env.ALLOWED_ORIGINS_LOCAL!,
+  process.env.ALLOWED_ORIGINS_PROD!,
+];
+
+const options: cors.CorsOptions = { origin: allowedOrigins };
+
 app.disable("x-powered-by");
+
+app.use(cors(options));
 
 app.use(express.json());
 app.use(morgan("dev"));
